b0dad21a627b7cd114747e69f2197383e90c8e70
lushenglei <1152644850@qq.com>
2022-08-09 09:52:23 +0800

    第一次提交

---
+++ src/main/java/com/junyue/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.junyue.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/sayHello/{name}")
+    public String sayHello(@PathVariable String name){
+        return name+" said: Hello Jenkins";
+    }
+}
